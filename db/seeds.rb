bbd02b09317137002e1c627c5bc7b923cbbe3eca
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-02 23:42:06 -0600

    Seeded 5 new actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,26 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+actors = [
+  {
+    first_name: "Jeff",
+    last_name: "Bridges",
+    known_for: "The Big Lebowski"
+  },
+  {
+    first_name: "Viola",
+    last_name: "Davis",
+    known_for: "Fences"
+  },
+  {
+    first_name: "Kate",
+    last_name: "Winslet",
+    known_for: "Eternal Sunshine of the Spotless Mind"
+  }
+]
+
+actors.each do |actor|
+  Actor.create!(actor)
+end
+
+puts "Seeded #{Actor.count} actors 🎉"
