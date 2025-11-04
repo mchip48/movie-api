4ed2a5364e0e2b630e0299faef06d7ca953a4d25
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-03 21:15:23 -0600

    Added first test for controller index

--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ActorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/actors.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Actor.count, data.count
+  end
 end
