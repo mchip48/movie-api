69ea6f766988d781eddb74a6140cce3165921fd9
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-04 19:19:56 -0600

    added tests for show, create, update, destroy in actors_controller_test & changed put to patch in routes for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   get "/actors" => "actors#index"
   get "/actors/:id" => "actors#show"
   post "/actors" => "actors#create"
-  put "actors/:id" => "actors#update"
+  patch "actors/:id" => "actors#update"
   delete "actors/:id" => "actors#destroy"
 end
--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -8,4 +8,35 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Actor.count, data.count
   end
+  
+  test "show" do
+    get "/actors/#{Actor.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "first_name", "last_name", "known_for"], data.keys
+  end
+
+  test "create" do
+    assert_difference "Actor.count", 1 do
+      post "/actors.json", params: { first_name: "Matthew", last_name: "Chipkin", known_for: "Actualize" }
+      assert_response 200
+    end
+  end
+
+  test "update" do
+    actor = Actor.first
+    patch "/actors/#{actor.id}.json", params: { first_name: "Updated First Name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated First Name", data["first_name"]
+  end
+
+  test "destroy" do
+    assert_difference "Actor.count", -1 do
+      delete "/actors/#{Actor.first.id}.json"
+      assert_response 200
+    end
+  end
 end
