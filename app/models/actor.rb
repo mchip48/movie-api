db5a3f6d81cb90d081bab5c799ca78f82e33c024
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-09 22:21:54 -0600

    Added Movie model and migrated to database, and removed gender:string from actors database

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,8 @@
 class Actor < ApplicationRecord
+
+  def create
+    actor = Actor.new(
+      first_name: params[:first_name]
+    )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_06_015750) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_10_042015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_06_015750) do
     t.string "known_for"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "gender"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "plot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
